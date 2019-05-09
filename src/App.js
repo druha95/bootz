0cbfee7355222c9a4aa67d979df88a25d5d8edfd
Andrii Medvedskyi <amedvedsky@cogniance.com>
2019-05-09 21:31:48 +0300

    add logo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import logo from './assets/img/logo.svg';
+import './App.scss';
 
 const AppRouter = () => {
     return (
         <div>
             <Router>
                 <div>
+                    <div className={'logo'}>
+                        <img className={'logo__image'} src={logo} alt="Bootz"/>
+                    </div>
                     <nav className={'navbar navbar-dark bg-dark'}>
                         <ul className={'navbar-nav'}>
                             <li className={'nav-item active'}>
-                                <Link className={'nav-link'} to='/'>Products</Link>
+                                <Link className={'nav-link'} to='/'>All Products</Link>
                             </li>
                         </ul>
                     </nav>
 
-                    <div className={'container-fluid'}>
+                    <div className={'container'}>
                         <Route path='/' component={MainPage} />
                     </div>
                 </div>
